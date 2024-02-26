a119393b8fbc12bc106b6127b15fd180734cdc36
umerali34119 <123723411+umerali34119@users.noreply.github.com>
2024-02-26 20:26:58 +0500

    ../

--- slider.js
+++ slider.js
@@ -8,3 +8,36 @@ $(document).ready(() => {
 
 
 })
+
+
+
+function sidebar() {
+    var menuBar = document.querySelector('.searchside');
+    if (menuBar) {
+        if (!menuBar.classList.contains('sideBar')) {
+            menuBar.classList.add('sideBar');
+        } else {
+            menuBar.classList.remove('sideBar');
+        }
+    } 
+}
+function sidebar1() {
+    var menuBar = document.querySelector('.userside');
+    if (menuBar) {
+        if (!menuBar.classList.contains('sideBar')) {
+            menuBar.classList.add('sideBar');
+        } else {
+            menuBar.classList.remove('sideBar');
+        }
+    } 
+}
+function sidebar2() {
+    var menuBar = document.querySelector('.shoppingside');
+    if (menuBar) {
+        if (!menuBar.classList.contains('sideBar')) {
+            menuBar.classList.add('sideBar');
+        } else {
+            menuBar.classList.remove('sideBar');
+        }
+    } 
+}
